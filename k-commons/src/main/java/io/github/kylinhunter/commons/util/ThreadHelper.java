56a175ca68d87eea15fbf4bf6c0234ca7ec9a606
kylinhunter <250917500@qq.com>
2022-12-29 02:08:49 +0800

    init

--- k-commons/src/main/java/io/github/kylinhunter/commons/util/ThreadHelper.java
+++ k-commons/src/main/java/io/github/kylinhunter/commons/util/ThreadHelper.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
  * @author: BiJi'an
  * @create: 2021/8/19
  **/
-public class TheadHelper {
+public class ThreadHelper {
     public static void sleep(int count, TimeUnit timeUnit) {
         try {
             timeUnit.sleep(count);
--- k-commons/src/test/java/io/github/kylinhunter/commons/cache/guava/AbstractCacheTest.java
+++ k-commons/src/test/java/io/github/kylinhunter/commons/cache/guava/AbstractCacheTest.java
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
 
-import io.github.kylinhunter.commons.util.TheadHelper;
+import io.github.kylinhunter.commons.util.ThreadHelper;
 
 public class AbstractCacheTest {
 
@@ -19,7 +19,7 @@ public class AbstractCacheTest {
 
             value = guavaCache.get(new Integer(1), new Integer(2));
             System.out.println(value);
-            TheadHelper.sleep(100, TimeUnit.MILLISECONDS);
+            ThreadHelper.sleep(100, TimeUnit.MILLISECONDS);
         }
         guavaCache.put(100L, new Integer(1), new Integer(2));
         value = guavaCache.get(new Integer(1), new Integer(2));
@@ -41,7 +41,7 @@ public class AbstractCacheTest {
 
             value = guavaCache.get(new TestParam1("1"), new TestParam2("parm2"));
             System.out.println(value);
-            TheadHelper.sleep(100, TimeUnit.MILLISECONDS);
+            ThreadHelper.sleep(100, TimeUnit.MILLISECONDS);
         }
         guavaCache.put(100L, new TestParam1("1"), new TestParam2("parm2"));
         value = guavaCache.get(new TestParam1("1"), new TestParam2("parm2"));
