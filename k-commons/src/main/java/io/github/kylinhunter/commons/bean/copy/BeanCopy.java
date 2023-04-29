148ca4df2f691b8a9596a5c2d9691c05167c8844
kylinhunter <250917500@qq.com>
2023-04-30 00:43:22 +0800

    init

--- k-commons/src/main/java/io/github/kylinhunter/commons/bean/copy/BeanCopy.java
+++ k-commons/src/main/java/io/github/kylinhunter/commons/bean/copy/BeanCopy.java
@@ -1,9 +1,8 @@
 package io.github.kylinhunter.commons.bean.copy;
 
-import java.util.List;
-
 import io.github.kylinhunter.commons.bean.copy.convertor.ConvertExcetion;
 import io.github.kylinhunter.commons.bean.copy.convertor.FieldConvertor;
+import java.util.List;
 import lombok.Setter;
 
 /**
@@ -13,28 +12,29 @@ import lombok.Setter;
  **/
 
 public class BeanCopy {
-    @Setter
-    private List<FieldConvertor> fieldConvertors;
 
-    public boolean isEmpty() {
-        return (fieldConvertors == null || fieldConvertors.isEmpty());
+  @Setter
+  private List<FieldConvertor> fieldConvertors;
+
+  public boolean isEmpty() {
+    return (fieldConvertors == null || fieldConvertors.isEmpty());
+  }
+
+  /**
+   * @param source source
+   * @param target target
+   * @return void
+   * @title copy
+   * @description
+   * @author BiJi'an
+   * @date 2022-11-19 01:14
+   */
+  public void copy(Object source, Object target) throws ConvertExcetion {
+
+    if (fieldConvertors != null && !fieldConvertors.isEmpty()) {
+      for (FieldConvertor fieldConvertor : fieldConvertors) {
+        fieldConvertor.convert(source, target);
+      }
     }
-
-    /**
-     * @param source source
-     * @param target target
-     * @return void
-     * @title copy
-     * @description
-     * @author BiJi'an
-     * @date 2022-11-19 01:14
-     */
-    public void copy(Object source, Object target) throws ConvertExcetion {
-
-        if (fieldConvertors != null && !fieldConvertors.isEmpty()) {
-            for (FieldConvertor fieldConvertor : fieldConvertors) {
-                fieldConvertor.convert(source, target);
-            }
-        }
-    }
+  }
 }
