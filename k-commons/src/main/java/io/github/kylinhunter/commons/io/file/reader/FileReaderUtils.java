8e0e14cd0c7ebc955c93e22c3a32afbaf0877201
kylinhunter <250917500@qq.com>
2023-06-10 01:50:10 +0800

    init

--- k-commons/src/main/java/io/github/kylinhunter/commons/io/file/reader/FileReaderUtils.java
+++ k-commons/src/main/java/io/github/kylinhunter/commons/io/file/reader/FileReaderUtils.java
@@ -1,6 +1,5 @@
 package io.github.kylinhunter.commons.io.file.reader;
 
-import io.github.kylinhunter.commons.exception.embed.KIOException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,8 +7,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Objects;
+
 import org.apache.commons.io.Charsets;
 import org.apache.commons.io.IOUtils;
+
+import io.github.kylinhunter.commons.exception.ExCatcher;
+import io.github.kylinhunter.commons.exception.embed.KIOException;
 
 /**
  * @author BiJi'an
@@ -18,97 +21,99 @@ import org.apache.commons.io.IOUtils;
  */
 public class FileReaderUtils {
 
-  /**
-   * @param file a file
-   * @param encoding the encoding of the file
-   * @param processor a processor to process the file
-   * @title process
-   * @description
-   * @author BiJi'an
-   * @date 2022-01-01 01:48
-   */
-  public static void process(File file, String encoding, LineProcessor processor) {
+    /**
+     * @param file      a file
+     * @param encoding  the encoding of the file
+     * @param processor a processor to process the file
+     * @title process
+     * @description
+     * @author BiJi'an
+     * @date 2022-01-01 01:48
+     */
+    public static void process(File file, String encoding, LineProcessor processor) {
 
-    try (InputStream input = new FileInputStream(file)) {
-      process(input, encoding, processor);
-    } catch (Exception e) {
-      throw new KIOException("process error", e);
+        try (InputStream input = new FileInputStream(file)) {
+            process(input, encoding, processor);
+        } catch (Exception e) {
+            throw new KIOException("process error", e);
+        }
     }
-  }
 
-  /**
-   * @param input the input stream
-   * @param encoding the encoding
-   * @param processor the processor
-   * @title process
-   * @description
-   * @author BiJi'an
-   * @date 2022-01-01 02:00
-   */
-  public static void process(InputStream input, String encoding, LineProcessor processor) {
+    /**
+     * @param input     the input stream
+     * @param encoding  the encoding
+     * @param processor the processor
+     * @title process
+     * @description
+     * @author BiJi'an
+     * @date 2022-01-01 02:00
+     */
+    public static void process(InputStream input, String encoding, LineProcessor processor) {
 
-    try (InputStreamReader streamReader =
-            new InputStreamReader(input, Charsets.toCharset(encoding));
-        BufferedReader bufferReader = new BufferedReader(streamReader)) {
-      String line = bufferReader.readLine();
-      while (line != null) {
-        processor.process(line);
-        line = bufferReader.readLine();
-      }
-    } catch (Exception e) {
-      throw new KIOException("process error", e);
+        try (InputStreamReader streamReader = new InputStreamReader(input, Charsets.toCharset(encoding));
+             BufferedReader bufferReader = new BufferedReader(streamReader)) {
+            String line = bufferReader.readLine();
+            while (line != null) {
+                processor.process(line);
+                line = bufferReader.readLine();
+            }
+        } catch (Exception e) {
+            throw new KIOException("process error", e);
+        }
     }
-  }
 
-  /**
-   * @param file a file
-   * @param encoding the encoding of the file
-   * @param processor a processor to process the file
-   * @title process
-   * @description
-   * @author BiJi'an
-   * @date 2022-01-01 01:48
-   */
-  @SuppressWarnings("UnusedReturnValue")
-  public static <T> T process(File file, String encoding, DefaultLineProcessor<T> processor) {
+    /**
+     * @param file      a file
+     * @param encoding  the encoding of the file
+     * @param processor a processor to process the file
+     * @title process
+     * @description
+     * @author BiJi'an
+     * @date 2022-01-01 01:48
+     */
+    @SuppressWarnings("UnusedReturnValue")
+    public static <T> T process(File file, String encoding, DefaultLineProcessor<T> processor) {
 
-    try (InputStream input = new FileInputStream(file)) {
-      process(input, encoding, processor);
-      return processor.getResult();
-    } catch (Exception e) {
-      throw new KIOException("process error", e);
+        try (InputStream input = new FileInputStream(file)) {
+            process(input, encoding, processor);
+            return processor.getResult();
+        } catch (Exception e) {
+            throw new KIOException("process error", e);
+        }
     }
-  }
 
-  /**
-   * @param file file
-   * @return byte[]
-   * @title readFileToByteArray
-   * @description
-   * @author BiJi'an
-   * @date 2023-04-22 00:32
-   */
-  public static byte[] readFileToByteArray(final File file) throws IOException {
-    try (InputStream inputStream = openInputStream(file)) {
-      final long fileLength = file.length();
-      // file.length() may return 0 for system-dependent entities, treat 0 as unknown length - see
-      // IO-453
-      return fileLength > 0
-          ? IOUtils.toByteArray(inputStream, fileLength)
-          : IOUtils.toByteArray(inputStream);
+    /**
+     * @param file file
+     * @return byte[]
+     * @title readFileToByteArray
+     * @description
+     * @author BiJi'an
+     * @date 2023-04-22 00:32
+     */
+    public static byte[] readFileToByteArray(final File file) {
+        try (InputStream inputStream = openInputStream(file)) {
+            final long fileLength = file.length();
+            // file.length() may return 0 for system-dependent entities, treat 0 as unknown length - see
+            // IO-453
+            return fileLength > 0 ? IOUtils.toByteArray(inputStream, fileLength) : IOUtils.toByteArray(inputStream);
+        } catch (IOException e) {
+            throw new KIOException("readFileToByteArray error", e);
+        }
     }
-  }
 
-  /**
-   * @param file file
-   * @return java.io.FileInputStream
-   * @title openInputStream
-   * @description
-   * @author BiJi'an
-   * @date 2023-04-22 00:32
-   */
-  public static FileInputStream openInputStream(final File file) throws IOException {
-    Objects.requireNonNull(file, "file");
-    return new FileInputStream(file);
-  }
+    /**
+     * @param file file
+     * @return java.io.FileInputStream
+     * @title openInputStream
+     * @description
+     * @author BiJi'an
+     * @date 2023-04-22 00:32
+     */
+    public static FileInputStream openInputStream(final File file) {
+        return ExCatcher.run(() -> {
+            Objects.requireNonNull(file, "file");
+            return new FileInputStream(file);
+        });
+
+    }
 }
--- k-commons/src/test/java/io/github/kylinhunter/commons/io/file/reader/FileReaderUtilsTest.java
+++ k-commons/src/test/java/io/github/kylinhunter/commons/io/file/reader/FileReaderUtilsTest.java
@@ -2,23 +2,29 @@ package io.github.kylinhunter.commons.io.file.reader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import io.github.kylinhunter.commons.io.ResourceHelper;
 import java.io.File;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import io.github.kylinhunter.commons.io.ResourceHelper;
 
 class FileReaderUtilsTest {
 
-  @Test
-  void process() {
-    String path = "classpath:/test/file/test1.txt";
-    File distFile = ResourceHelper.getFile(path);
-    AtomicInteger lines = new AtomicInteger(0);
-    FileReaderUtils.process(distFile, "UTF-8", (line -> lines.incrementAndGet()));
-    assertEquals(2, lines.get());
+    @Test
+    void process() {
+        String path = "classpath:/test/file/test1.txt";
+        File distFile = ResourceHelper.getFile(path);
+        AtomicInteger lines = new AtomicInteger(0);
+        FileReaderUtils.process(distFile, "UTF-8", (line -> lines.incrementAndGet()));
+        assertEquals(2, lines.get());
 
-    FileStatLineProcessor fileStatLinesProcessor = new FileStatLineProcessor();
-    FileReaderUtils.process(distFile, "UTF-8", fileStatLinesProcessor);
-    assertEquals(2, fileStatLinesProcessor.getResult().getLineNum());
-  }
+        FileStatLineProcessor fileStatLinesProcessor = new FileStatLineProcessor();
+        FileReaderUtils.process(distFile, "UTF-8", fileStatLinesProcessor);
+        assertEquals(2, fileStatLinesProcessor.getResult().getLineNum());
+
+        byte[] bytes = FileReaderUtils.readFileToByteArray(distFile);
+        Assertions.assertTrue(bytes.length > 0);
+    }
 }
