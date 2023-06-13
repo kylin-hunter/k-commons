f744ed0ecb2ca5da3ffde85013a83fd3c747bd2c
kylinhunter <250917500@qq.com>
2023-06-13 23:06:21 +0800

    init

--- k-commons/src/test/java/io/github/kylinhunter/commons/io/IOHelperTest.java
+++ k-commons/src/test/java/io/github/kylinhunter/commons/io/IOHelperTest.java
@@ -1,7 +1,6 @@
 package io.github.kylinhunter.commons.io;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URI;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,11 +8,15 @@ import org.junit.jupiter.api.Test;
 class IOHelperTest {
 
   @Test
-  void testToString() throws IOException {
+  void testToString() {
     String path = "/test/file/test1.txt";
     File file = ResourceHelper.getFileInClassPath(path);
     URI uri = IOHelper.toURI(file.getAbsolutePath());
     System.out.println("uri:" + uri);
-    Assertions.assertTrue(uri != null);
+    Assertions.assertNotNull(uri);
+
+    Assertions.assertThrows(
+        RuntimeException.class,
+        () -> IOHelper.toURI(null));
   }
 }
