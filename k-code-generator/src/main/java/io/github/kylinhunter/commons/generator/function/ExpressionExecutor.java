60e908ca3a8ddc592a722fd367c1dfa98d023a9f
kylinhunter <250917500@qq.com>
2023-06-15 00:19:20 +0800

    init

--- k-code-generator/src/main/java/io/github/kylinhunter/commons/generator/function/ExpressionExecutor.java
+++ k-code-generator/src/main/java/io/github/kylinhunter/commons/generator/function/ExpressionExecutor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2023 The k-commons Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.kylinhunter.commons.generator.function;
 
 import com.googlecode.aviator.AviatorEvaluator;
@@ -35,7 +50,7 @@ public class ExpressionExecutor {
 
   /**
    * @param expression expression
-   * @param type       type
+   * @param type type
    * @return T
    * @title execute
    * @description
@@ -48,7 +63,7 @@ public class ExpressionExecutor {
 
   /**
    * @param expression expression
-   * @param env        env
+   * @param env env
    * @return T
    * @title execute
    * @description
@@ -63,8 +78,8 @@ public class ExpressionExecutor {
 
   /**
    * @param expression expression
-   * @param env        env
-   * @param type       type
+   * @param env env
+   * @param type type
    * @return T
    * @title execute
    * @description
@@ -91,7 +106,7 @@ public class ExpressionExecutor {
 
   /**
    * @param path path
-   * @param env  env
+   * @param env env
    * @param type type
    * @return T
    * @title executeFromFile
--- k-utils/src/main/java/o/github/kylinhunter/commons/utils/yaml/YamlHelper.java
+++ k-utils/src/main/java/o/github/kylinhunter/commons/utils/yaml/YamlHelper.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2023 The k-commons Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package o.github.kylinhunter.commons.utils.yaml;
 
 import io.github.kylinhunter.commons.exception.common.KRuntimeException;
@@ -32,7 +47,7 @@ public class YamlHelper {
 
   /**
    * @param clazz clazz
-   * @param path  path
+   * @param path path
    * @return T
    * @title loadFromClassPath
    * @description
@@ -45,7 +60,7 @@ public class YamlHelper {
 
   /**
    * @param clazz clazz
-   * @param path  path
+   * @param path path
    * @return T
    * @title loadFromClassPath
    * @description
@@ -71,7 +86,7 @@ public class YamlHelper {
 
   /**
    * @param clazz clazz
-   * @param text  text
+   * @param text text
    * @return T
    * @title loadFromText
    * @description
@@ -84,7 +99,7 @@ public class YamlHelper {
 
   /**
    * @param clazz clazz
-   * @param text  text
+   * @param text text
    * @return T
    * @title loadFromText
    * @description
@@ -115,7 +130,7 @@ public class YamlHelper {
   }
 
   /**
-   * @param obj      obj
+   * @param obj obj
    * @param nameRule nameRule
    * @return java.lang.String
    * @title dumpAsMap
