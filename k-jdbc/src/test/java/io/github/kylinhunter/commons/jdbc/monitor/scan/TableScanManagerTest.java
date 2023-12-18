023a091a23695c43124a2e009e4ca9b2ee98671d
kylinhunter <250917500@qq.com>
2023-12-19 00:01:09 +0800

    init

--- k-jdbc/src/test/java/io/github/kylinhunter/commons/jdbc/monitor/scan/TableScanManagerTest.java
+++ k-jdbc/src/test/java/io/github/kylinhunter/commons/jdbc/monitor/scan/TableScanManagerTest.java
@@ -2,7 +2,6 @@ package io.github.kylinhunter.commons.jdbc.monitor.scan;
 
 import io.github.kylinhunter.commons.jdbc.TestHelper;
 import io.github.kylinhunter.commons.jdbc.datasource.DataSourceManager;
-import io.github.kylinhunter.commons.jdbc.monitor.scan.ScanTable.ScanTableBuilder;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 
@@ -14,18 +13,23 @@ class TableScanManagerTest {
     TestHelper.initTestSQl(dataSource);
     TableScanManager manager = new TableScanManager(dataSource);
 
-    ScanTableBuilder builder = ScanTable.builder();
-    builder.tableName(TestHelper.TEST_TABLE_ROLE1)
-        .tablePkName("id")
-        .tableTimeName("sys_auto_updated")
-        .destination(TestHelper.MONITOR_SCAN_TASK)
-        .scanLimit(100)
-        .scanInterval(-1);
-//
-//    ScanTable scanTable = builder.build();
-//    manager.init(scanTable);
-//    manager.clean(scanTable);
-//    manager.start();
+    TableScanConfig tableScanConfig = new TableScanConfig();
+    tableScanConfig.setServerId("1");
+    tableScanConfig.setScheduleCorePoolSize(1);
+
+    ScanTable scanTable1 = TestTableScanManager.getScanTable();
+    scanTable1.setTableName(TestHelper.TEST_TABLE_ROLE1);
+    scanTable1.setScanInterval(-1);
+    tableScanConfig.add(scanTable1);
+
+    ScanTable scanTable2 = TestTableScanManager.getScanTable();
+    scanTable2.setTableName(TestHelper.TEST_TABLE_ROLE2);
+    scanTable1.setScanInterval(-1);
+    tableScanConfig.add(scanTable2);
+
+    manager.init(tableScanConfig);
+    manager.clean();
+    manager.start();
 
   }
 }
