b812f8dcb041b2a3defb702558d6c860bac84240
kylinhunter <250917500@qq.com>
2023-12-20 00:22:52 +0800

    init

--- k-jdbc/src/test/java/io/github/kylinhunter/commons/jdbc/monitor/binlog/BinLogTableMonitorTest.java
+++ k-jdbc/src/test/java/io/github/kylinhunter/commons/jdbc/monitor/binlog/BinLogTableMonitorTest.java
@@ -1,10 +1,29 @@
 package io.github.kylinhunter.commons.jdbc.monitor.binlog;
 
+import io.github.kylinhunter.commons.collections.ListUtils;
+import io.github.kylinhunter.commons.jdbc.binlog.BinLogClient;
+import io.github.kylinhunter.commons.jdbc.binlog.BinLogConfig;
+import io.github.kylinhunter.commons.jdbc.binlog.TestBinLogClient;
+import io.github.kylinhunter.commons.reflect.ReflectUtils;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class BinLogTableMonitorTest {
 
   @Test
   void start() {
+
+    BinLogConfig binLogConfig = TestBinLogClient.getBinLogConfig();
+
+    BinLogTableMonitor binLogTableMonitor = new BinLogTableMonitor(binLogConfig);
+
+    MonitorTable monitorTable1 = TestBinLogTableMonitor.getMonitorTable1();
+    MonitorTable monitorTable2 = TestBinLogTableMonitor.getMonitorTable2();
+    binLogTableMonitor.setMonitorTables(ListUtils.newArrayList(monitorTable1, monitorTable2));
+    BinLogClient binLogClient = Mockito.mock(BinLogClient.class);
+    ReflectUtils.setField(binLogTableMonitor, "binLogClient", binLogClient);
+
+    binLogTableMonitor.reset();
+    binLogTableMonitor.start();
   }
 }
